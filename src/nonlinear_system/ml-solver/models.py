79476f639551db312cc1796e4e878b5f04141714
Charlie Ardern <93714899+charlieardern@users.noreply.github.com>
2026-01-22 20:53:26 +0000

    Enhanced ml solver, still kind of garbage

--- src/nonlinear_system/ml-solver/models.py
+++ src/nonlinear_system/ml-solver/models.py
@@ -10,7 +10,40 @@ class SimpleNeuralField(nn.Module):
             torch.nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
             nn.Linear(hidden_dim, 1)
             )
-    def forward(self, x):
-        return self.mlp(x)
+
+    def forward(self, x, L, s_G, s_init):
+        bc_L_func = L-x[:,:,0].unsqueeze(-1)
+        ic_func = (x[:,:,1].unsqueeze(-1))
+        print(f"s_init.shape: {s_init.shape}")
+        print(f"self.mlp(x)*bc_L_func*ic_func.shape: {(self.mlp(x)*bc_L_func*ic_func).shape}")
+        return self.mlp(x)*bc_L_func*ic_func+s_init
+
+class FeatureEngineeredNeuralField(nn.Module):
+    def __init__(self, hidden_dim):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(5, hidden_dim),
+            torch.nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            nn.Linear(hidden_dim, 1)
+            )
+
+    def forward(self, x, L, s_G, s_init):
+        X = torch.concatenate([x,x**2, (x[:,:,0]*x[:,:,1]).unsqueeze(-1)],dim=-1)
+        s = self.mlp(X)
+        bc_L_func = L-x[:,:,0].unsqueeze(-1)
+        ic_func = (x[:,:,1].unsqueeze(-1))
+        print(f"s_init.shape: {s_init.shape}")
+        print(f"self.mlp(x)*bc_L_func*ic_func.shape: {(s*bc_L_func*ic_func).shape}")
+        return s*bc_L_func*ic_func+s_init
--- src/nonlinear_system/ml-solver/train.py
+++ src/nonlinear_system/ml-solver/train.py
@@ -1,9 +1,10 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 from timeit import default_timer
 
-from models import SimpleNeuralField
+from models import FeatureEngineeredNeuralField
 
 from tqdm.auto import tqdm
 
@@ -26,7 +27,6 @@ t = torch.linspace(0,t_final, steps=num_t_steps, requires_grad=True).reshape(1,-
 def s_0(x, s_G):
     return -s_G*x/L + 2*s_G
 s_t0 = s_0(x[:,0,0].reshape(dim,1,1), s_G).detach()
-print(f"req grad: {s_t0.requires_grad}")
 
 # Spacetime grid of points at which to evaluate model:
 X = torch.concatenate([x,t], dim=2)
@@ -34,18 +34,19 @@ X = torch.concatenate([x,t], dim=2)
 # Bedrock at spacetime points
 bed = torch.zeros(dim, requires_grad=True).reshape(dim,1,1).repeat(1,num_t_steps,1)
 
-model = SimpleNeuralField(hidden_dim=64)
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+model = FeatureEngineeredNeuralField(hidden_dim=256)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.002)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.5)
 
 train_time_start = default_timer()
-epochs = 200
+epochs = 400
 losses = []
 
 for epoch in tqdm(range(epochs)):
     print(f"Epoch: {epoch+1}\n-----------")
     model.train()
 
-    s_pred = model(X)
+    s_pred = model(X,L, s_G, s_t0)
     deriv_1 = torch.autograd.grad(
         outputs=s_pred,
         inputs=X,
@@ -64,11 +65,11 @@ for epoch in tqdm(range(epochs)):
 
     # Both of shape (1, num_t_steps, 2):
     boundary_0 = torch.concatenate([torch.zeros(num_t_steps).reshape(1,num_t_steps,1),t[0].reshape(1,num_t_steps,1)],axis=2)
-    boundary_L = torch.concatenate([L*torch.ones(num_t_steps).reshape(1,num_t_steps,1),t[0].reshape(1,num_t_steps,1)],axis=2)
+    #boundary_L = torch.concatenate([L*torch.ones(num_t_steps).reshape(1,num_t_steps,1),t[0].reshape(1,num_t_steps,1)],axis=2)
     #print(f"boundary_L.shape: {boundary_L.shape}")
 
-    boundary_L_err = 0.01*torch.sum((model(boundary_L)-s_G)**2)
-    boundary_0_values = model(boundary_0)
+    #boundary_L_err = 0.01*torch.sum((model(boundary_L)-s_G)**2)
+    boundary_0_values = model(boundary_0, L, s_G, s_0(torch.zeros(1), s_G).reshape(1,1,1))
     boundary_0_deriv = torch.autograd.grad(
         outputs=boundary_0_values,
         inputs=boundary_0,
@@ -77,29 +78,32 @@ for epoch in tqdm(range(epochs)):
         grad_outputs = torch.ones_like(boundary_0_values),
     )[0]
 
-    boundary_0_err = torch.sum(boundary_0_deriv[:,:,0]**2)
+    boundary_0_err = 0.5*torch.sum(boundary_0_deriv[:,:,0]**2)
 
     init_X = torch.concatenate([x[:,0,0].reshape(dim,1,1),torch.zeros_like(s_t0)],dim=2)
 
-    s0_pred = model(init_X)
-    s_0_mse = 0.01*torch.sum((s0_pred-s_t0)**2)
+    #s0_pred = model(init_X, L, s_G, s_t0)
+    #s_0_mse = 0.01*torch.sum((s0_pred-s_t0)**2)
 
     f_err = deriv_1[:,:,1]-(g/(3*eta))*deriv_2[:,:,0]-a
     mse_f = 0.001*torch.sum(f_err**2)
     print(f"mse_f: {mse_f}")
     print(f"boundary_0_err: {boundary_0_err}")
-    print(f"boundary_L_err: {boundary_L_err}")
-    print(f"s_0_mse: {s_0_mse}")
-    loss = mse_f + boundary_0_err + boundary_L_err + s_0_mse
+    #print(f"boundary_L_err: {boundary_L_err}")
+    #print(f"s_0_mse: {s_0_mse}")
+    loss = mse_f + boundary_0_err #+ s_0_mse boundary_L_err + 
     print(f"loss: {loss}")
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    losses.append(loss.values)
+    scheduler.step()
+    losses.append(loss.item())
 
 model.eval()
-s = model(X).detach().cpu().numpy()
+s = model(X, L, s_G, s_t0).detach().cpu().numpy()
+plt.plot(losses)
+plt.savefig("figures/loss_curves.png")
 
 print(s.shape)
 
