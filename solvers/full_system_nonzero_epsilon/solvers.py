Fixed t scaling error in shelf solver

solvers/full_system_nonzero_epsilon/solvers.py
@@ -65,9 +65,9 @@ def numerical_fv(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, r
         H_G_arr = np.array(H_G_list)*B*C
         q_G_arr = np.array(q_G_list)*B**3/2
         x_G_dot_arr = np.array(x_G_dot_list)*q_0/(B*C)
-        t_G_arr = np.array(t_G_list)*2*B**2*C**5
-        dt_scaled = delta_t*2*B**2*C**5
-        t_scaled = t*2*B**2*C**5
+        t_G_arr = np.array(t_G_list)*2*B**2*C**5/(a*L)
+        dt_scaled = delta_t*2*B**2*C**5/(a*L)
+        t_scaled = t*2*B**2*C**5/(a*L)
 
         for iii in range(1+round((len(x_G_list))/num_microsteps)):
             idx = num_microsteps*iii
@@ -129,9 +129,10 @@ def numerical_fv(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, r
     for i in tqdm(range(num_steps)):
         # Calculate shelf:
         t = i*num_microsteps*delta_t
-        x_shelf, H_shelf = compute_shelf(t, num_microsteps, x_G_full_list, H_G_list, q_G_list, x_G_dot_list, t_G_list)
-        x_shelf_list.append(np.pad(x_shelf, (0, 100-len(x_shelf)), mode='edge').reshape(-1,1))
-        H_shelf_list.append(np.pad(H_shelf, (0, 100-len(H_shelf)), mode='edge').reshape(-1,1))
+        if not test_mode:
+            x_shelf, H_shelf = compute_shelf(t, num_microsteps, x_G_full_list, H_G_list, q_G_list, x_G_dot_list, t_G_list)
+            x_shelf_list.append(np.pad(x_shelf, (0, 100-len(x_shelf)), mode='edge').reshape(-1,1))
+            H_shelf_list.append(np.pad(H_shelf, (0, 100-len(H_shelf)), mode='edge').reshape(-1,1))
 
         # Calculate sheet:
         for ii in range(num_microsteps):
@@ -147,6 +148,9 @@ def numerical_fv(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, r
             H_G_list.append(H_G)
             q_G_list.append(-g/(3*nu)*H_G**3*((8*eps*x_G+h[-2]-9*h[-1])*N/(3*x_G)))
             t_G_list.append(i*num_microsteps*delta_t+(ii+1)*delta_t)
+
+            if np.isnan(np.array(h)).any():
+                print("Solution has diverged.")
 
         h_list.append(h.reshape(-1,1))
         x_G_list.append(x_G)
@@ -165,6 +169,7 @@ def numerical_fv(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, r
         converge = True
 
     converge = not np.isnan(h_tnsr).any()
+    print(f"Converged: {converge}")
 
     if test_mode:
         out = converge, x_G_tnsr