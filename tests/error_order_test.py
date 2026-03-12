6c75321c1c71466eafb5289a022bc0f18d4627b3
Charlie Ardern <charlie.ardern@gmail.com>
2026-03-12 21:35:53 +0000

    Fixed error order tests finally!

--- tests/error_order_test.py
+++ tests/error_order_test.py
@@ -3,13 +3,23 @@ from solvers.solvers import numerical_fv_implicit
 import matplotlib.pyplot as plt
 import os
 from tqdm.auto import tqdm
+from scipy.optimize import curve_fit
 
-#N = 100
+def quadratic(x, a, b, c):
+    return a*x**2+b*x + c
+
+time_test = True
+compute_fine_time = False
+
+space_test = True
+compute_fine_space = False
+
 x_G_0 = 0.3
-nu = 80
+#nu = 80
+nu=300
 num_steps = 100
 #num_microsteps = 12000
-t_final = 200
+t_final = 100
 rho_w = 1000
 rho = 917
 g = 9.81
@@ -18,7 +28,7 @@ L = 150
 
 q_0 = a*L
 g_prime = g*(rho_w-rho)/rho_w
-alpha = 0.5*np.sqrt(g_prime/g) # alpha required for A=1
+alpha = 0.24#0.5*np.sqrt(g_prime/g) # alpha required for A=1
 print(alpha)
 
 A = 2*alpha*np.sqrt(g/g_prime)
@@ -31,60 +41,96 @@ print("Computing error order tests...")
 folder = "figures/"
 os.makedirs(folder, exist_ok=True)
 
-# Run test for delta_t order -------------------------------------------
-microstep_list = [1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]
-rmse_list = []
-N = 200
-h_0 = 1-np.linspace(0,1,num=N)+0.002+0.1
-# _, h_fine, _, _ = numerical_fv_implicit(h_0, 10000, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
-# h_fine = h_fine[:,-1]/(B*C)
+if time_test:
+    # Run test for delta_t order -------------------------------------------
+    microstep_list = [14, 15, 16, 17, 20, 40, 80, 90, 100]
+    fine_microsteps = 1000
+    rmse_list = []
+    N = 1000
+    h_0 = 1-np.linspace(0,1,num=N)+0.002+0.1
+    # _, h_fine, _, _ = numerical_fv_implicit(h_0, 10000, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
+    # h_fine = h_fine[:,-1]/(B*C)
 
-for i in tqdm(range(len(microstep_list))):
-    num_microsteps = microstep_list[i]
-    x, h, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True, sheet_accumulation=False)
-    
-    h_fine = 1 - x[:,-1]/(2*B*C**4)
-    
-    # Non-dimensionalise h:
-    h_pred = h[:,-1]/(B*C)
-    rmse_list.append(np.sqrt(np.mean((h_pred-h_fine)**2)))
-    print(f"rmse: {rmse_list[i]}")
+    if compute_fine_time:
+        h_0 = 1-np.linspace(0,1,num=N)+0.002+0.1
+        _, h_fine, _, _ = numerical_fv_implicit(h_0, fine_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
+        h_fine = h_fine[:,-1]/(B*C)
+        os.makedirs("saved_objects/", exist_ok=True)
+        np.save("saved_objects/fine_solution_t.npy", h_fine)
+    else:
+        h_fine = np.load("saved_objects/fine_solution_t.npy")
 
-dt_scaled_values = (a*L/(2*B**2*C**5))*t_final/(num_steps*np.array(microstep_list))
-plt.figure(figsize=(8,5), dpi=300)
-plt.plot(dt_scaled_values, rmse_list)
-plt.title("RMSE for steady state", fontname="Latin Modern Roman", fontsize=16)
-plt.xlabel(r"$\Delta \hat t$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
-plt.ylabel(r"$RMSE_h$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
-plt.savefig(folder + "rmse_vs_dt.png")
-plt.close()
+    for i in tqdm(range(len(microstep_list))):
+        num_microsteps = microstep_list[i]
+        x, h, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
+
+        
+        # Non-dimensionalise h:
+        h_pred = h[:,-1]/(B*C)
+        rmse_list.append(np.sqrt(np.mean((h_pred-h_fine)**2)))
+        print(f"rmse: {rmse_list[i]}")
+
+    dt_scaled_values = (a*L/(2*B**2*C**5))*t_final/(num_steps*np.array(microstep_list))
+    plt.figure(figsize=(8,5), dpi=300)
+    plt.scatter(dt_scaled_values, rmse_list)
+
+    popt, pcov = curve_fit(quadratic, dt_scaled_values, rmse_list)
+    plt.plot(dt_scaled_values, quadratic(dt_scaled_values,*popt))
+
+    #plt.plot(np.log(dt_scaled_values), np.log(rmse_list))
+    plt.title("RMSE for steady state", fontname="Latin Modern Roman", fontsize=16)
+    plt.xlabel(r"$\Delta \hat t$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
+    plt.ylabel(r"$RMSE_h$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
+    plt.savefig(folder + "rmse_vs_dt.png")
+    plt.close()
+
+    
+    print(f"Temporal quadratic weight: {popt[0]} +- {np.sqrt(pcov[0,0])}")
+    print(f"Temporal linear weight: {popt[1]} +- {np.sqrt(pcov[1,1])}")
 
 # Run test for delta_chi order -------------------------------------------
-N_list = [5, 10, 20, 40, 80, 160, 320, 640]
-num_microsteps = 50000
-# _, h_fine, _, _ = numerical_fv_implicit(h_0, 10000, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
-# h_fine = h_fine[:,-1]/(B*C)
-rmse_list = []
+if space_test:
+    t_final = 100
+    num_microsteps = 1000
+    N_values = np.round(1/np.linspace(1/5, 1/100, num=30))
+    #N_values = np.round(np.linspace(5,100, num=30))
+    N_fine = 1000
+    j = np.linspace(0,N_fine-1, num=N_fine)
+    chi_fine = (j+0.5)/N_fine
 
-h_0 = 1-np.linspace(0,1,num=1000)+0.002+0.1
-x, h_fine, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True, sheet_accumulation=False)
-h_fine = h_fine[:,-1]/(B*C)
+    if compute_fine_space:
+        h_0 = 1-np.linspace(0,1,num=N_fine)+0.002+0.1
+        _, h_fine, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
+        h_fine = h_fine[:,-1]/(B*C)
+        os.makedirs("saved_objects/", exist_ok=True)
+        np.save("saved_objects/fine_solution.npy", h_fine)
+    else:
+        h_fine = np.load("saved_objects/fine_solution.npy")
 
-for i in tqdm(range(len(N_list))):
-    N = N_list[i]
-    h_0 = 1-np.linspace(0,1,num=N)+0.002+0.1
-    x, h, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True, sheet_accumulation=False)
-    # h_fine = 1 - x[:,-1]/(2*B*C**4)
-    h_pred = h[:,-1]/(B*C)
-    rmse_list.append(np.sqrt(np.mean((h_pred-h_fine)**2)))
-    print(f"rmse: {rmse_list[i]}")
+    rmse_list = []
 
-plt.figure(figsize=(8,5), dpi=300)
-plt.plot(1/np.array(N_list), rmse_list)
-plt.title("RMSE for steady state", fontname="Latin Modern Roman", fontsize=16)
-plt.xlabel(r"$\Delta \hat \chi$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
-plt.ylabel(r"$RMSE_h$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
-plt.savefig(folder + "rmse_vs_dchi.png")
-plt.close()
+    for i in tqdm(range(len(N_values))):
+        N = int(N_values[i])
+        j = np.linspace(0,N-1, num=N)
+        chi_new = (j+0.5)/N
+
+        h_0 = 1-np.linspace(0,1,num=N)+0.002+0.1
+        x, h, _, _ = numerical_fv_implicit(h_0, num_microsteps, num_steps, t_final, x_G_0, g, nu, rho_w, rho, alpha, a, L, test_mode=True)
+        h_pred = h[:,-1]/(B*C)
+        h_fine_i = np.interp(chi_new, chi_fine, h_fine)
+
+        rmse_list.append(np.sqrt(np.mean((h_pred-h_fine_i)**2)))
+        print(f"rmse: {rmse_list[i]}")
+
+    plt.figure(figsize=(8,5), dpi=300)
+    plt.plot(1/np.array(N_values), rmse_list)
+    plt.title("RMSE for steady state", fontname="Latin Modern Roman", fontsize=16)
+    plt.xlabel(r"$\Delta \hat \chi$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
+    plt.ylabel(r"$RMSE_h$ (dimensionless)", fontname = "Latin Modern Roman", fontsize=14)
+    plt.savefig(folder + "rmse_vs_dchi.png")
+    plt.close()
+    popt, pcov = curve_fit(quadratic, 1/np.array(N_values), rmse_list)
+    print(f"Spatial quadratic weight: {popt[0]} +- {np.sqrt(pcov[0,0])}")
+    print(f"Spatial linear weight: {popt[1]} +- {np.sqrt(pcov[1,1])}")
 
 print("Complete.")
